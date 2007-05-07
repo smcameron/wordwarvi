1075a4416a15763df403cedfcfbc853f980fcaa2
smcameron <smcameron>
2007-05-07 23:35:09 +0000

    Added algorithm to generate fractal terrain and scroll through it.  249 lines of C.

--- wordwarvi.c
+++ wordwarvi.c
@@ -1,11 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <gtk/gtk.h>
+
+#define TERRAIN_LENGTH 2000
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+#define WORLDWIDTH (SCREEN_WIDTH * 40)
+
+#define ROUGHNESS (0.35)
+
+struct terrain_t {
+	int npoints;
+	int x[TERRAIN_LENGTH];
+	int y[TERRAIN_LENGTH];
+} terrain;
+
+struct game_state_t {
+	int x;
+	int y;
+	int last_x1, last_x2;
+	int velocity;
+} game_state = { 0, 0, 0, 0, 10 };
+
+
+GdkGC *gc = NULL;
+GtkWidget *main_da;
+gint timer_tag;
+
+void perturb(int *value, int lower, int upper, double percent)
+{
+	double perturbation;
+
+	perturbation = percent * (lower - upper) * ((0.0 + random()) / (0.0 + RAND_MAX) - 0.5);
+	*value += perturbation;
+	return;
+}
+
+void generate_sub_terrain(struct terrain_t *t, int xi1, int xi2)
+{
+	int midxi;
+	int y1, y2, y3, tmp;
+	int x1, x2, x3;
+
+
+	midxi = (xi2 - xi1) / 2 + xi1;
+	if (midxi == xi2 || midxi == xi1)
+		return;
+
+	y1 = t->y[xi1];
+	y2 = t->y[xi2];
+	if (y1 > y2) {
+		tmp = y1;
+		y1 = y2;
+		y2 = tmp;
+	}
+	y3 = ((y2 - y1) / 2) + y1;
+
+	x1 = t->x[xi1];
+	x2 = t->x[xi2];
+	if (x1 > x2) {
+		tmp = x1;
+		x1 = x2;
+		x2 = tmp;
+	}
+	x3 = ((x2 - x1) / 2) + x1;
+	
+	perturb(&x3, x2, x1, ROUGHNESS);
+	perturb(&y3, x2, x1, ROUGHNESS);
+
+	t->x[midxi] = x3;
+	t->y[midxi] = y3;
+	printf("gst %d %d\n", x3, y3);
+
+	generate_sub_terrain(t, xi1, midxi);
+	generate_sub_terrain(t, midxi, xi2);
+}
+
+void generate_terrain(struct terrain_t *t)
+{
+	t->npoints = TERRAIN_LENGTH;
+	t->x[0] = 0;
+	t->y[0] = 0;
+	t->x[t->npoints-1] = WORLDWIDTH;
+	t->y[t->npoints-1] = t->y[0];
+
+	generate_sub_terrain(t, 0, t->npoints-1);
+}
+
+
+static int main_da_expose(GtkWidget *w, GdkEvent *event, gpointer p)
+{
+	int i;
+	int sx1, sx2;
+	int zz = 0;
+
+	sx1 = game_state.x - SCREEN_WIDTH / 3;
+	sx2 = game_state.x + 4*SCREEN_WIDTH/3;
+	for (i=0;i<terrain.npoints-1;i++) { /* optimize this later */
+		if (terrain.x[i] < sx1 && terrain.x[i+1] < sx1) /* offscreen to the left */
+			continue;
+		if (terrain.x[i] > sx2 && terrain.x[i+1] > sx2) /* offscreen to the right */
+			continue;
+		if (zz < 20) {
+			if (game_state.y < terrain.y[i+1] - 150)
+				game_state.y += 3;
+			if (game_state.y > terrain.y[i+1] - 50)
+				game_state.y -= 3;
+			zz++;
+		}
+		 gdk_draw_line(w->window, gc, terrain.x[i] - game_state.x, terrain.y[i]+(SCREEN_HEIGHT/2) - game_state.y,  
+					 terrain.x[i+1] - game_state.x, terrain.y[i+1]+(SCREEN_HEIGHT/2) - game_state.y);
+	}
+	return 0;
+}
 
 /* This is a callback function. The data arguments are ignored
  * in this example. More on callbacks below. */
 static void hello( GtkWidget *widget,
                    gpointer   data )
 {
-    g_print ("Hello World\n");
+    g_print ("Bye bye.\n");
 }
 
 static gboolean delete_event( GtkWidget *widget,
@@ -33,17 +147,25 @@ static void destroy( GtkWidget *widget,
     gtk_main_quit ();
 }
 
-#define SCREEN_WIDTH 600
-#define SCREEN_HEIGHT 400
+gint advance_game(gpointer data)
+{
+	game_state.x += game_state.velocity;
+	gtk_widget_queue_draw(main_da);
+	if (WORLDWIDTH - game_state.x < 100)
+		return FALSE;
+	else
+		return TRUE;
+}
+
 
 int main( int   argc,
           char *argv[] )
 {
+
     /* GtkWidget is the storage type for widgets */
     GtkWidget *window;
     GtkWidget *button;
     GtkWidget *vbox;
-    GtkWidget *main_da;
 
 	GdkColor whitecolor;
 	GdkColor bluecolor;
@@ -77,12 +199,14 @@ int main( int   argc,
     /* Sets the border width of the window. */
     gtk_container_set_border_width (GTK_CONTAINER (window), 10);
    
-    vbox = gtk_vbox_new(FALSE, 0); 
+	vbox = gtk_vbox_new(FALSE, 0); 
 	main_da = gtk_drawing_area_new();
 	gtk_widget_modify_bg(main_da, GTK_STATE_NORMAL, &whitecolor);
+	gtk_widget_set_size_request(main_da, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    /* Creates a new button with the label "Hello World". */
-    button = gtk_button_new_with_label ("Hello World");
+	g_signal_connect(G_OBJECT (main_da), "expose_event", G_CALLBACK (main_da_expose), NULL);
+
+    button = gtk_button_new_with_label ("Quit");
     
     /* When the button receives the "clicked" signal, it will call the
      * function hello() passing it NULL as its argument.  The hello()
@@ -98,23 +222,23 @@ int main( int   argc,
                               G_OBJECT (window));
     
     /* This packs the button into the window (a gtk container). */
-    gtk_container_add(GTK_CONTAINER (vbox), main_da);
-    gtk_container_add (GTK_CONTAINER (vbox), button);
     gtk_container_add (GTK_CONTAINER (window), vbox);
 
-	gtk_widget_set_size_request(main_da, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-    gtk_box_pack_start(window, vbox, TRUE /* expand */, TRUE /* fill */, 2);
-    gtk_box_pack_start(vbox, main_da, TRUE /* expand */, FALSE /* fill */, 2);
-    gtk_box_pack_start(vbox, button, FALSE /* expand */, FALSE /* fill */, 2);
+    gtk_box_pack_start(GTK_BOX (vbox), main_da, TRUE /* expand */, FALSE /* fill */, 2);
+    gtk_box_pack_start(GTK_BOX (vbox), button, FALSE /* expand */, FALSE /* fill */, 2);
     
     /* The final step is to display this newly created widget. */
     gtk_widget_show (vbox);
     gtk_widget_show (main_da);
     gtk_widget_show (button);
+
+    generate_terrain(&terrain);
     
     /* and the window */
     gtk_widget_show (window);
+	gc = gdk_gc_new(GTK_WIDGET(main_da)->window);
+
+    timer_tag = g_timeout_add(30, advance_game, NULL);
     
     /* All GTK applications must have a gtk_main(). Control ends here
      * and waits for an event to occur (like a key press or
