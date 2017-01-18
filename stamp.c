f3bac4345eb7cdba6e5199968047337ccdf453b0
Stephen M. Cameron <stephenmcameron@gmail.com>
2017-01-17 19:31:33 -0800

    Check that SOURCE_DATE_EPOCH is convertible to uint64_t
    Make stamp fail if not, and print a hint why.
    
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- stamp.c
+++ stamp.c
@@ -2,6 +2,7 @@
 #include <sys/time.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <inttypes.h>
 
 int main(int argc, char *argv[])
 {
@@ -14,8 +15,18 @@ int main(int argc, char *argv[])
 
 	source_date_epoch = getenv("SOURCE_DATE_EPOCH");
 	if (source_date_epoch) {
+		int n;
+		uint64_t value;
+
+		n = sscanf(source_date_epoch, "%" PRIu64, &value);
+		if (n != 1) {
+			fprintf(stderr, "SOURCE_DATE_EPOCH environment variable value '%s' is not convertible to a uint64_t\n",
+				source_date_epoch);
+			fprintf(stderr, "Either make sure it is unset, or set it to seconds since midnight, Jan 1, 1970.\n");
+			exit(1);
+		}
 		printf("static int builder = 0;\n");
-		printf("static uint64_t builtat = %s;\n", source_date_epoch);
+		printf("static uint64_t builtat = %" PRIu64 "\n", value);
 		exit(0);
 	}
 
