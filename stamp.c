2690a07cacdd05fd629cb08c572be8bfedb48c8d
Chris Lamb <chris@chris-lamb.co.uk>
2017-01-18 11:24:34 +1100

    Make the build reproducible if SOURCE_DATE_EPOCH is set.

--- stamp.c
+++ stamp.c
@@ -7,9 +7,17 @@ int main(int argc, char *argv[])
 {
 	struct timeval tv;
 	int uid;
+	char *source_date_epoch;
 
 	gettimeofday(&tv, NULL);
 	uid = getuid();
+
+	source_date_epoch = getenv("SOURCE_DATE_EPOCH");
+	if (source_date_epoch) {
+		printf("static int builder = 0;\n");
+		printf("static uint64_t builtat = %s;\n", source_date_epoch);
+		exit(0);
+	}
 
 	printf("static int builder = %d;\n", uid);
 	printf("static uint64_t builtat = %d;\n", tv.tv_sec);
